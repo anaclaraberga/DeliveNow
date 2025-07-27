6b50ae40e473ff69b7dc93662e8af5069a4736a9
anaclaraberga <ana4ew.br@gmail.com>
2025-07-27 11:26:32 -0300

    Inserted: Project files

---
+++ Microservices/Delivery-Tracking/src/test/java/br/com/delivenow/delivery/tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delivenow.delivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
