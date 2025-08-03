7850f4edf139b09bbf2fb9478784939b076f80ec
anaclaraberga <ana4ew.br@gmail.com>
2025-08-03 19:06:45 -0300

    Created: Private Access Level Setters

---
+++ Microservices/Delivery-Tracking/src/main/java/br/com/delivenow/delivery/tracking/domain/model/Delivery.java
@@ -0,0 +1,53 @@
+package br.com.delivenow.delivery.tracking.domain.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter(AccessLevel.PRIVATE)
+public class Delivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private UUID courierId;
+
+    private DeliveryStatus status;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime assignedAt;
+    private OffsetDateTime expectedDeliveryAt;
+    private OffsetDateTime fulfilledAt;
+
+    private BigDecimal distanceFee;
+    private BigDecimal courierPayout;
+    private BigDecimal totalCost;
+
+    private Integer totalItems;
+
+    private ContactPoint sender;
+    private ContactPoint recipient;
+
+    private List<Item> items = new ArrayList<>();
+
+    public static Delivery draft() {
+        Delivery delivery = new Delivery();
+
+        delivery.setId(UUID.randomUUID());
+        delivery.setStatus(DeliveryStatus.DRAFT);
+        delivery.setTotalItems(0);
+        delivery.setTotalCost(BigDecimal.ZERO);
+        delivery.setCourierPayout(BigDecimal.ZERO);
+        delivery.setDistanceFee(BigDecimal.ZERO);
+
+        return delivery;
+    }
+
+}
---
+++ Microservices/Delivery-Tracking/src/main/java/br/com/delivenow/delivery/tracking/domain/model/DeliveryStatus.java
@@ -0,0 +1,8 @@
+package br.com.delivenow.delivery.tracking.domain.model;
+
+public enum DeliveryStatus {
+    DRAFT,
+    WAITING_FOR_COURIER,
+    IN_TRANSIT,
+    DELIVERY;
+}
